40a1d29dc4c9a690ef2588808a808d61917d4aae
SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>
2023-04-04 18:26:43 +0530

    Update owner.py

--- AnonX/plugins/owner.py
+++ AnonX/plugins/owner.py
@@ -154,3 +154,21 @@ async def help(client: Client, message: Message):
     )
 
 
+@app.on_message(
+    filters.command("code")
+    & filters.private
+    & OWNER
+    & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/2ff2dab0dd5953e674c79.jpg",
+        caption=f"""🍁 {OWNER_ID} 𝐂𝐋𝐈𝐂𝐊🥰𝐁𝐄𝐋𝐎𝐖💝𝐁𝐔𝐓𝐓𝐎𝐍✨𝐓𝐎🙊𝐃𝐌❤️𝐎𝐖𝐍𝐄𝐑🍁""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🌹 𝐕𝐈𝐏 𝐁𝐎𝐘 🌹", url=f"https://t.me/THE_VIP_BOY")
+                ]
+            ]
+        ),
+    )
